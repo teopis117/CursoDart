a5118ebc64bb846fb1a2ac53932ab4cf6e59cdfa
Jose Diaz <lalo_dimo_teopis_@hotmail.com>
2022-10-14 12:07:44 -0500

    terminamos el curso viendo como es la creacion de una aplicacion y las diferentes dependencias y como es un proyecto que podemos crear

---
+++ 9.-paquetes/dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,21 @@
+//el 42 sale de importar donde lo unico que hace es resolver 6*7=42
+//al colocar un alias hacemos referencia de donde llamaremos funciones que ocupemos
+/*
+como ordenar las importaciones
+1.-de dart
+2.-de la documentacion
+3.-creacion propia
+*/
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main(List<String> arguments) {
+  final url = 'https://reqres.in/api/users?page=2';
+  //  get(url.then(res))
+  {
+// print(res);
+  }
+  ;
+}
